e685df76b1d3607b968e47b994bb5a0d9868b052
matisarralde <mati.sarralde@gmail.com>
2025-10-22 22:15:22 -0300

    Update calendar.tsx

--- apps/frontend/app/components/ui/calendar.tsx
+++ apps/frontend/app/components/ui/calendar.tsx
@@ -1,1 +1,9 @@
+"use client";
+import * as React from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+
+export function Calendar(props: React.ComponentProps<typeof DayPicker>) {
+  return <DayPicker {...props} />;
+}
 
