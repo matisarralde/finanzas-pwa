713af60aa1cf8c4e8ecaac7b47aaebb1130cbe5a
matisarralde <mati.sarralde@gmail.com>
2025-10-22 22:16:18 -0300

    Update utils.ts

--- apps/frontend/app/components/lib/utils.ts
+++ apps/frontend/app/components/lib/utils.ts
@@ -1,1 +1,7 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)); }
+export const formatCurrency = (value: number | undefined | null) =>
+  new Intl.NumberFormat("es-CL", { style: "currency", currency: "CLP", maximumFractionDigits: 0 })
+    .format(value ?? 0);
 
